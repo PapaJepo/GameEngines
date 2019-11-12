3cf179d4eea14b14b98fda992992d0746ae6ec9e
PapaJepo <44029409+PapaJepo@users.noreply.github.com>
2019-11-12 15:15:35 +0000

    Basic Control Script and Camera

---
+++ GameEngines_Assignment/Assets/Scripts/CameraLerp.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraLerp : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.Lerp(transform.position, cameraTarget.position, Time.deltaTime * 2.0f);
+        transform.LookAt(cameraTarget.parent);
+    }
+}
