0a85ebadeef8507b72921598f2317826697a3f9a
PapaJepo <44029409+PapaJepo@users.noreply.github.com>
2019-11-24 22:23:37 +0000

    Audio Testing

---
+++ GameEngines_Assignment/Assets/SpawnCubes.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnCubes : MonoBehaviour
+{
+    public GameObject _sampleCubePrefab;
+    GameObject[] _sampleCubes = new GameObject[128];
+    public float CircleRadius;
+    public float _MaxScale;
+    public AudioPeer AudioRef;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i <128; i ++)
+        {
+            GameObject _instanceSampleCube = (GameObject)Instantiate(_sampleCubePrefab);
+            _instanceSampleCube.transform.position = this.transform.position;
+            _instanceSampleCube.transform.parent = this.transform;
+            _instanceSampleCube.name = "SampleCube" + i;
+            this.transform.eulerAngles = new Vector3(0, -2.8125f * i, 0);
+            //this.transform.rotation = new Quaternion(0, 0, 25,1);
+            _instanceSampleCube.transform.position = Vector3.forward * 1f;
+
+            _sampleCubes[i] = _instanceSampleCube;
+
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for(int i = 0;i < 128;i++)
+        {
+            if(_sampleCubes != null)
+            {
+                _sampleCubes[i].transform.localScale = new Vector3(0.6f, (AudioRef._samples[i] * _MaxScale) + 0.5f, 0.6f);
+            }
+        }
+    }
+}
--- GameEngines_Assignment/Assets/WormHole/DissplaceSphere.cs
+++ GameEngines_Assignment/Assets/WormHole/DissplaceSphere.cs
@@ -7,10 +7,9 @@ public class DissplaceSphere : MonoBehaviour
     public GameObject MainSong;
     public float displacementAmount;
     MeshRenderer meshRender;
-    public float[] m_audioSpectrum;
     private AudioSource m_Audio;
 
-    private float[] spectrum = new float[1024];
+   
     // Start is called before the first frame update
     void Start()
     {
@@ -24,16 +23,8 @@ public class DissplaceSphere : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // AudioListener.GetSpectrumData(m_audioSpectrum, 0, FFTWindow.Hamming);
-        //displacementAmount = m_Audio.pitch * 1.1f;
-        m_Audio.GetSpectrumData(spectrum, 0, FFTWindow.Rectangular);
-
-        for (int i = 0; i < spectrum.Length; i++)
-        {
-            displacementAmount = spectrum[i] * 10000;
-
-        }
-        //displacementAmount = Mathf.Lerp(displacementAmount, 0, Time.deltaTime);
+       
+        displacementAmount = m_Audio.volume *9;
         meshRender.material.SetFloat("_Amount", displacementAmount);
     }
 
