36fe14c1b7e0b65cb48c064fda959c60968c46d8
PapaJepo <44029409+PapaJepo@users.noreply.github.com>
2019-11-24 15:56:26 +0000

    Update Wormhole.cs

--- GameEngines_Assignment/Assets/WormHole/Wormhole.cs
+++ GameEngines_Assignment/Assets/WormHole/Wormhole.cs
@@ -18,6 +18,75 @@ public class Wormhole : MonoBehaviour
         return p;
 
     }
+
+    private Mesh mesh;
+    private Vector3[] vertices;
+    private int[] triangles;
+
+
+    private void Awake()
+    {
+        GetComponent<MeshFilter>().mesh = mesh = new Mesh();
+        mesh.name = "Pipe";
+        SetVertices();
+        SetTriangles();
+    }
+
+    private void SetVertices()
+    {
+        vertices = new Vector3[pipeSegementCount * curveSegmentCount * 4];
+        float uStep = (2f * Mathf.PI) / curveSegmentCount;
+        CreateFirstQuadRing(uStep);
+        int iDelta = pipeSegementCount * 4;
+        for(int u = 2, i = iDelta; u<=curveSegmentCount;u++, i+= iDelta)
+        {
+            CreateQuadRing(u * uStep, i);
+        }
+        mesh.vertices = vertices;
+    }
+
+    private void CreateQuadRing(float u, int i)
+    {
+        float vStep = (2f * Mathf.PI) / pipeSegementCount;
+        int ringOffset = pipeSegementCount * 4;
+
+        Vector3 vertex = GetPointOnTorus(u, 0f);
+        for(int v = 1; v <= pipeSegementCount; v++, i+= 4)
+        {
+            vertices[i] = vertices[i - ringOffset + 2];
+            vertices[i + 1] = vertices[i - ringOffset + 3];
+            vertices[i + 2] = vertex;
+            vertices[i + 3] = vertex = GetPointOnTorus(u, v * vStep);
+        }
+    }
+
+    private void CreateFirstQuadRing(float u)
+    {
+        float vStep = (2f * Mathf.PI) / pipeSegementCount;
+
+        Vector3 vertexA = GetPointOnTorus(0f, 0f);
+        Vector3 vertexB = GetPointOnTorus(u, 0f);
+        for(int v = 1,i = 0; v <= pipeSegementCount; v++,i+=4)
+        {
+            vertices[i] = vertexA;
+            vertices[i+1] = vertexA = GetPointOnTorus(0f, v * vStep);
+            vertices[i + 2] = vertexB;
+            vertices[i + 3] = vertexB = GetPointOnTorus(u, v * vStep);
+        }
+    }
+
+    private void SetTriangles()
+    {
+        triangles = new int[pipeSegementCount * curveSegmentCount * 6];
+        for(int t = 0, i = 0; t < triangles.Length; t+= 6, i+=4)
+        {
+            triangles[t] = i;
+            triangles[t + 1] = triangles[t + 4] = i + 1;
+            triangles[t + 2] = triangles[t + 3] = i + 2;
+            triangles[t + 5] = i + 3;
+        }
+        mesh.triangles = triangles;
+    }
     // Start is called before the first frame update
     void Start()
     {
