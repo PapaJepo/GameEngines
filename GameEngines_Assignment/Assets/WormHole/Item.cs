41dda931594a8c2598cb50ce2cbdaea9a336ef04
PapaJepo <44029409+PapaJepo@users.noreply.github.com>
2019-11-24 17:58:36 +0000

    Deleting test textures

---
+++ GameEngines_Assignment/Assets/WormHole/Item.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+
+    private Transform rotater;
+
+    private void Awake()
+    {
+        rotater = transform.GetChild(0);
+    }
+
+    public void Position(Wormhole hole, float curveRotation, float ringRotation)
+    {
+        //transform.SetParent(hole.prefab)
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
